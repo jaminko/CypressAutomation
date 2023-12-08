48c090f5f14c870598f8fee923a4ef5de709a7dc
Ivan Mocherniuk <jaminko@gmail.com>
2023-12-08 17:42:38 +0200

    Added API tests & disabled video recording

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false, // for Google drop-down tests
   reporter: 'cypress-mochawesome-reporter', // for generating HTML report
+  video: false, // disable video recording
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on); // for generating HTML report
---
+++ cypress/e2e/API/httpRequests.cy.js
@@ -0,0 +1,45 @@
+describe('HTTP Requests examples', () => {
+    it('GET call', () => {
+        cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1')
+        .its('status')
+        .should('eq', 200);
+    });
+
+    it('POST call', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            body: {
+                title: "Tets post",
+                body: "This is post call",
+                userId: 1
+            }
+        })
+        .its('status')
+        .should('equal', 201);
+    });
+
+    it('PUT call', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'https://jsonplaceholder.typicode.com/posts/1',
+            body: {
+                title: "Tets post",
+                body: "This is post call",
+                userId: 1,
+                id: 174102
+            }
+        })
+        .its('status')
+        .should('equal', 200);
+    });
+
+    it('DELETE call', () => {
+        cy.request({
+            method: 'DELETE',
+            url: 'https://jsonplaceholder.typicode.com/posts/1',
+        })
+        .its('status')
+        .should('equal', 200);
+    });
+});
